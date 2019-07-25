b5869a917f6f3ab3b197af7771aaba22ebf0ab4c
Vinton Teo <taulago@ymail.com>
2019-07-25 16:14:46 -0600

    Console log getMaps fetch data.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import './App.scss';
+import { getMaps } from '../../utils/apiCalls';
 
 export class App extends Component {
   componentDidMount = () => {
     console.log('component mounted...');
+    getMaps().then(data => console.log(data));
   };
 
   render() {
