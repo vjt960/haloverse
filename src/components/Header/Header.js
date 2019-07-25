348aae71ca1e9f40579695369210878c2fa22ae6
Vinton Teo <taulago@ymail.com>
2019-07-25 17:50:51 -0600

    Structure Header component with h1, navLinks.

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Header.scss';
+
+export const Header = () => {
+  return (
+    <Header>
+      <h1>Halo-verse Network</h1>
+      <nav className="header_nav">
+        <NavLink className="header_nav-link" exact to="/">
+          Home
+        </NavLink>
+        <NavLink className="header_nav-link" exact to="/halo_5">
+          Halo 5
+        </NavLink>
+        <NavLink className="header_nav-link" exact to="/halo_wars_2">
+          Halo Wars 2
+        </NavLink>
+      </nav>
+    </Header>
+  );
+};
