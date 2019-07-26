830e35e73dee40f13771f244fd35c38e9735a29a
Vinton Teo <taulago@ymail.com>
2019-07-26 10:04:08 -0600

    Set up 404 fallback component.

---
+++ src/components/Main/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import Showcase from '../../containers/Showcase/Showcase';
+// import Form from '../../containers/Form/Form';
+// import Profile from '../../containers/Profile/Profile';
+
+export const Main = () => {
+  return (
+    <main>
+      <h1>Main Component</h1>
+      {/* <Showcase />
+      <Form />
+      <Profile /> */}
+    </main>
+  );
+};
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { getMaps } from '../../utils/apiCalls';
 import Header from '../../components/Header/Header';
+import { Main } from '../../components/Main/Main';
+import { NotFound } from '../../components/NotFound/NotFound';
+import { Route, Switch } from 'react-router-dom';
 import './App.scss';
 
 export class App extends Component {
@@ -13,6 +16,10 @@ export class App extends Component {
     return (
       <div className="App">
         <Header />
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route component={NotFound} />
+        </Switch>
       </div>
     );
   }
