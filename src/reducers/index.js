69a3af6f486532aad9f6e8a791034ab12b75a493
Vinton Teo <taulago@ymail.com>
2019-07-27 15:20:03 -0600

    Create loadingReducer to handle isLoading.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { loadingReducer } from './loadingReducer';
+import { mapsReducer } from './mapsReducer';
 
 const rootReducer = combineReducers({
-  isLoading: loadingReducer
+  isLoading: loadingReducer,
+  maps: mapsReducer
 });
 
 export default rootReducer;
--- src/reducers/loadingReducer.js
+++ src/reducers/loadingReducer.js
@@ -1,5 +1,9 @@
-export const loadingReducer = (state = true, action) => {
+export const loadingReducer = (state = false, action) => {
   switch (action.type) {
+    case 'START_LOADING':
+      return true;
+    case 'END_LOADING':
+      return false;
     default:
       return state;
   }
