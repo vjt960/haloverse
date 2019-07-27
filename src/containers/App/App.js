66f3e8f6e27b745b5899f8ac7f80668035490c5c
Vinton Teo <taulago@ymail.com>
2019-07-27 15:28:55 -0600

    Restructure App to conditionally render Switch.

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,26 +1,50 @@
-import React, { Component } from 'react';
-import { getMaps } from '../../utils/apiCalls';
-import Header from '../../components/Header/Header';
-import { Main } from '../../components/Main/Main';
-import { NotFound } from '../../components/NotFound/NotFound';
+import { connect } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
+import React, { Component } from 'react';
+import { formatMapData } from '../../utils/cleaner';
+import { getMaps } from '../../utils/apiCalls';
+import Main from '../../components/Main/Main';
+import { NotFound } from '../../components/NotFound/NotFound';
+import { startLoading, endLoading, storeMaps } from '../../actions';
+import Header from '../../components/Header/Header';
 import './App.scss';
 
 export class App extends Component {
   componentDidMount = () => {
-    console.log('component mounted...');
-    getMaps().then(data => console.log(data));
+    const { startLoading, endLoading, storeMaps } = this.props;
+    startLoading();
+    getMaps()
+      .then(data => formatMapData(data))
+      .then(maps => storeMaps(maps))
+      .then(() => endLoading());
   };
 
   render() {
     return (
       <div className="App">
         <Header />
-        <Switch>
-          <Route exact path="/" component={Main} />
-          <Route component={NotFound} />
-        </Switch>
+        {this.props.maps ? (
+          <Switch>
+            <Route exact path="/" component={Main} />
+            <Route component={NotFound} />
+          </Switch>
+        ) : null}
       </div>
     );
   }
 }
+
+const mapStateToProps = state => ({
+  maps: state.maps
+});
+
+const mapDispatchToProps = dispatch => ({
+  startLoading: () => dispatch(startLoading()),
+  endLoading: () => dispatch(endLoading()),
+  storeMaps: maps => dispatch(storeMaps(maps))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
