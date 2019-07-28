328fabebd623259cfe6b02285ba0b5a7abab334e
Vinton Teo <taulago@ymail.com>
2019-07-28 13:01:25 -0600

    Create function to format fetched stats data; regex iso format handler.

--- src/utils/cleaner.js
+++ src/utils/cleaner.js
@@ -8,3 +8,36 @@ export const formatMapData = mapData => {
   });
   return maps.filter(map => map.description !== null);
 };
+
+export const formatStats = data => {
+  const record = data.Results.shift().Result;
+  const info = record.ArenaStats;
+  const rank = record.SpartanRank;
+  const xp = record.Xp;
+  return {
+    rank,
+    xp,
+    kills: info.TotalKills,
+    deaths: info.TotalDeaths,
+    wins: info.TotalGamesWon,
+    losses: info.TotalGamesLost,
+    ties: info.TotalGamesTied,
+    headshots: info.TotalHeadshots,
+    spartanKills: info.TotalSpartanKills,
+    shots: info.TotalShotsFired,
+    hits: info.TotalShotsLanded,
+    damage: info.TotalWeaponDamage,
+    time: formatTimeStamp(info.TotalTimePlayed),
+    medals: info.MedalAwards.length
+  };
+};
+
+const isoRegex = /(-)?P(?:([.,\d]+)Y)?(?:([.,\d]+)M)?(?:([.,\d]+)W)?(?:([.,\d]+)D)?T(?:([.,\d]+)H)?(?:([.,\d]+)M)?(?:([.,\d]+)S)?/;
+const formatTimeStamp = time => {
+  const matches = time.match(isoRegex);
+  return {
+    hours: matches[6] === undefined ? 0 : matches[6],
+    minutes: matches[7] === undefined ? 0 : matches[7],
+    seconds: matches[8] === undefined ? 0 : matches[8]
+  };
+};
