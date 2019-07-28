59b4de3d641c112de1c7208092d5a3e8b8dd60de
Vinton Teo <taulago@ymail.com>
2019-07-28 13:02:12 -0600

    Create function to fetch player emblem.

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -30,3 +30,20 @@ export const getSpartanImg = async gamertag => {
     throw Error(message);
   }
 };
+
+export const getEmblem = async gamertag => {
+  const url = `https://www.haloapi.com/profile/h5/profiles/${gamertag}/emblem?size=512`;
+  const cors_proxy = 'https://cors-anywhere.herokuapp.com/';
+  const options = { headers: { 'Ocp-Apim-Subscription-Key': apiKey } };
+  try {
+    const response = await fetch(cors_proxy + url, options);
+    if (!response.ok) {
+      throw new Error('Failed to retrieve Spartan image.');
+    }
+    const blob = await response.blob();
+    return URL.createObjectURL(blob);
+  } catch ({ message }) {
+    throw Error(message);
+  }
+};
+
