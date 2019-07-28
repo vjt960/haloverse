0896487dc449e74f871e090526c8b0ce232c63ea
Vinton Teo <taulago@ymail.com>
2019-07-27 20:50:30 -0600

    Add userReducer to store.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { userReducer } from './userReducer';
 import { loadingReducer } from './loadingReducer';
 import { mapsReducer } from './mapsReducer';
 
 const rootReducer = combineReducers({
+  user: userReducer,
   isLoading: loadingReducer,
   maps: mapsReducer
 });
