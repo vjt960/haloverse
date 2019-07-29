0b87e4c162e07cdf9cf5f07c9951c6fd31eea359
Vinton Teo <taulago@ymail.com>
2019-07-29 17:49:20 -0600

    Structure and pass tests for errorReducer.

---
+++ src/reducers/errorReducer.test.js
@@ -0,0 +1,24 @@
+import { errorReducer } from './errorReducer';
+
+describe('errorReducer', () => {
+  it('should return initial state', () => {
+    const expected = '';
+    const result = errorReducer(undefined, '');
+    expect(result).toEqual(expected);
+  });
+
+  it('should update emblem state with HAS_ERRORED', () => {
+    const state = '';
+    const mockError = 'Error: example text';
+    const action = { type: 'HAS_ERRORED', errorMessage: mockError };
+    const result = errorReducer(state, action);
+    expect(result).toEqual(mockError);
+  });
+
+  it('should update emblem state with CLEAR_ERROR', () => {
+    const state = '';
+    const action = { type: 'CLEAR_ERROR' };
+    const result = errorReducer(state, action);
+    expect(result).toEqual(state);
+  });
+});
