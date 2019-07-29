bec70afa0514a9b5a572140e74998f77f2625132
Vinton Teo <taulago@ymail.com>
2019-07-29 00:03:03 -0600

    Create Placeholder component.

---
+++ src/components/Placeholder.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Placeholder = () => {
+  return (
+    <section className="profile_container">
+      <h2 className="profile_gamertag">ENTER AN XBOX GAMERTAG TO VIEW STATS</h2>
+    </section>
+  );
+};
