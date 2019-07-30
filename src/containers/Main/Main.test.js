dbc7a75c8be47887d607bff37e0fe318b1116c4b
Vinton Teo <taulago@ymail.com>
2019-07-30 02:45:17 -0600

    Create and pass tests for Main component.

---
+++ src/containers/Main/Main.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Main, mapStateToProps } from './Main';
+
+describe('Main', () => {
+  it('should match snapshot', () => {
+    const wrapper = shallow(<Main maps={[1, 2, 3]} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should match snapshot when conditionally rendering Showcase', () => {
+    const altWrapper = shallow(<Main maps={{ length: false }} />);
+    expect(altWrapper).toMatchSnapshot();
+  });
+
+  it('should match snapshot when conditionally rendering Placeholder', () => {
+    const altWrapper = shallow(<Main maps={{ length: false }} stats={true} />);
+    expect(altWrapper).toMatchSnapshot();
+  });
+});
+
+describe('mapStateToProps', () => {
+  it('should return an array of map objects', () => {
+    const mockState = {
+      maps: ['map1', 'map2', 'map3']
+    };
+
+    const expected = {
+      maps: ['map1', 'map2', 'map3']
+    };
+    const mappedProps = mapStateToProps(mockState);
+    expect(mappedProps).toEqual(expected);
+  });
+
+  it('should return an object containing user stats', () => {
+    const mockState = {
+      stats: { one: 'one', two: 'two' }
+    };
+
+    const expected = {
+      stats: { one: 'one', two: 'two' }
+    };
+    const mappedProps = mapStateToProps(mockState);
+    expect(mappedProps).toEqual(expected);
+  });
+});
